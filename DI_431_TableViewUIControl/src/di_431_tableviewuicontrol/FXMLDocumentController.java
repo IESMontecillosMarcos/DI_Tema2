9342b29b1bd90be19d3723abd9fa854f9ad1640b
IESMontecillosMarcos <71645845+IESMontecillosMarcos@users.noreply.github.com>
2020-11-19 10:25:42 +0100

    Add files via upload

---
+++ DI_431_TableViewUIControl/src/di_431_tableviewuicontrol/FXMLDocumentController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package di_431_tableviewuicontrol;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Marcos Gonzalez Leon
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
