bbbf452902d026d96ba87dcc3f0034ec65a55a76
IESMontecillosMarcos <71645845+IESMontecillosMarcos@users.noreply.github.com>
2020-11-19 10:20:50 +0100

    Add files via upload

---
+++ DI_42_UserInterfaceFXML/src/di_42_userinterfacefxml/DI_42_UserInterfaceFXML.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package di_42_userinterfacefxml;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Marcos Gonzalez Leon
+ */
+public class DI_42_UserInterfaceFXML extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("FXML Example");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
